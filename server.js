83d8256141078cbba499872fdb2b2b47a9141ab5
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-09 15:57:44 -0700

    Add swagger, cors, and home reply

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 import Fastify from 'fastify'
-import { cards } from './cards.js'
+import fastifyCors from 'fastify-cors'
+import fastifySwagger from 'fastify-swagger'
+import {cards} from './cards.js'
 
 const fastify = Fastify({
 	logger: true
 })
 
+fastify.register(fastifySwagger, {
+	exposeRoute: true,
+	routePrefix: '/docs',
+	swagger: {
+		info: {
+			title: 'Moon Cards API',
+			description: 'The Moon Cards API for Thirteen Moons',
+			version: '1.0.0'
+		}
+	}
+})
+
 const PORT = process.env.PORT || 3000
+
+fastify.get('/', async (request, reply) => {
+	await reply.send('Welcome to the Thirteen Moons Official Card Game API')
+})
 
 fastify.get('/cards', async (request, reply) => {
 	await reply.send(cards)
+})
+
+fastify.register(fastifyCors, {
+	origin: '*',
+	methods: ['GET', 'POST', 'PUT', 'DELETE']
 })
 
 fastify.listen(PORT, function (err, address){
