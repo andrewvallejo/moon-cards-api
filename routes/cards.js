f16d39f5615de0d015ca8e60ab7770268664acf2
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-08 18:38:37 -0700

    Refactor code to be put into their own files

--- routes/cards.js
+++ routes/cards.js
@@ -1,78 +1,101 @@
-import {moonData} from '../moonData'
+import {getCard, getCards, postCard} from '../controller/cardController'
 
-// Options for get /cards
-const getCards = {
-	schema: {
-		response: {
-			200: {
-				type: 'object',
-				properties: {
-					cards: {
-						type: 'array',
-						items: {
-							type: 'object',
-							properties: {
-								id: {
-									type: 'string'
-								},
-								talent: {
-									type: 'string'
-								},
-								terms: {
-									type: 'string'
-								},
-								count: {
-									type: 'number'
-								}
-								// intervals: {
-								// 	type: 'string'
-								// }
-							}
-						}
-					}
-				}
-			}
+// Card Schema
+
+const card = {
+	type: 'object',
+	properties: {
+		id: {
+			type: 'string'
+		},
+		talent: {
+			type: 'string'
+		},
+		terms: {
+			type: 'string'
+		},
+		count: {
+			type: 'number'
+		},
+		intervals: {
+			type: 'string'
 		}
 	}
 }
 
-export const cardRoutes = (fastify, opts, next) => {
+// Schema for the cards
+const getAllCards = {
+	schema: {
+		response: {
+			200: {
+				type: 'array',
+				cards: card
+			}
+		}
+	},
+	handler: getCards
+}
+
+const getSingleCard = {
+	schema: {
+		response: {
+			200: card
+		}
+	},
+	handler: getCard
+}
+
+const addCard = {
+	schema: {
+		body: {
+			type: 'object',
+			required: ['talent', 'terms', 'count', 'interval'],
+			properties: {
+				talent: {
+					type: 'string'
+				},
+				terms: {
+					type: 'string'
+				},
+				count: {
+					type: 'number'
+				},
+				interval: {
+					type: 'string'
+				}
+			}
+		},
+		response: {
+			200: card
+		}
+	},
+	handler: postCard
+}
+
+export const cardRoutes = (fastify, opts, done) => {
 	// GET /cards - returns all cards
-	fastify.get('/api/cards', async (request, reply) => {
-		await reply.send(moonData)
-	})
+	fastify.get('/cards', getAllCards)
 
 	// GET /cards/:id - returns card with matching id
-	fastify.get('/api/cards/:id', getCards, async (request, reply) => {
-		const card = moonDatafind((card) => card.id === request.params.id)
-		if (card) {
-			await reply.send(card)
-		} else {
-			await reply.send(null)
-		}
-	})
+	fastify.get('/cards/:id', getSingleCard)
 
 	// POST /cards - creates a new card
-	fastify.post('/api/cards', async (request, reply) => {
-		const card = request.body
-		moonDatapush(card)
-		await reply.send(card)
-	})
+	fastify.post('/cards', addCard)
 
 	// PUT /cards/:id - updates card with matching id
-	fastify.put('/api/cards/:id', async (request, reply) => {
-		const card = request.body
-		const index = moonDatafindIndex((c) => c.id === request.params.id)
-		cards[index] = card
-		await reply.send(card)
-	})
+	// fastify.put('/api/cards/:id', async (request, reply) => {
+	// 	const card = request.body
+	// 	const index = moonCardsfindIndex((c) => c.id === request.params.id)
+	// 	cards[index] = card
+	// 	await reply.send(card)
+	// })
 
-	// DELETE /cards/:id - deletes card with matching id
-	fastify.delete('/api/cards/:id', async (request, reply) => {
-		const index = moonDatafindIndex((c) => c.id === request.params.id)
-		moonDatasplice(index, 1)
-		await reply.send(cards)
-	})
+	// // DELETE /cards/:id - deletes card with matching id
+	// fastify.delete('/api/cards/:id', async (request, reply) => {
+	// 	const index = moonCardsfindIndex((c) => c.id === request.params.id)
+	// 	moonCardssplice(index, 1)
+	// 	await reply.send(cards)
+	// })
 
-	next()
+	done()
 }
