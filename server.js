5f1339a160fe23ccc204dddf72b8baf2c54caf2b
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-08 18:37:33 -0700

    Add swagger documentation

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 import Fastify from 'fastify'
-import {cardRoutes} from './routes/cards.js'
+import fastifySwagger from 'fastify-swagger'
+import {cardRoutes} from './routes/cards'
 
 const fastify = Fastify({
 	logger: true
+})
+
+fastify.register(fastifySwagger, {
+	exposeRoute: true,
+	routePrefix: '/docs',
+	swagger: {
+		info: {
+			title: 'Moon Cards API',
+			description: 'The Moon Cards API for Thirteen Moons',
+			version: '1.0.0'
+		}
+	}
 })
 
 fastify.register(cardRoutes)
