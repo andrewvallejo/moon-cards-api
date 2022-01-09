70997d32d3c2087058bc97f1aeb9bec85653478a
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-08 19:03:58 -0700

    Add controllers for update and delete card

--- controller/cardController.js
+++ controller/cardController.js
@@ -15,9 +15,22 @@ export const getCards = (request, reply) => {
 	reply.send(moonCards)
 }
 
-export const postCard = (request, reply) => {
+export const addCard = (request, reply) => {
 	const card = request.body
 	card.id = nanoid()
 	moonCards.push(card)
 	reply.send(card)
+}
+
+export const updateCard = (request, reply) => {
+	const card = request.body
+	const index = moonCards.findIndex((c) => c.id === request.params.id)
+	moonCards[index] = card
+	reply.send(card)
+}
+
+export const deleteCard = (request, reply) => {
+	const index = moonCards.findIndex((c) => c.id === request.params.id)
+	moonCards.splice(index, 1)
+	reply.send(moonCards)
 }
