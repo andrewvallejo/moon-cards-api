e7cb456ae860351c339faedb5f7ad6df9548a0ea
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2022-01-09 18:39:16 -0700

    Update code to id

--- data/mooncards.js
+++ data/mooncards.js
@@ -1,363 +1,363 @@
 const mooncards = [
 	{
-		code: 'w1',
+		id: 'w1',
 		talent: 'wisdom',
 		terms: 'Read a spiritual book for',
 		count: 1,
 		intervals: 'chapters'
 	},
 	{
-		code: 'w2',
+		id: 'w2',
 		talent: 'wisdom',
 		terms: 'Ask someone older than you and take down',
 		count: 2,
 		intervals: 'notes'
 	},
 	{
-		code: 'w3',
+		id: 'w3',
 		talent: 'wisdom',
 		terms: 'Read health and wellness',
 		count: 3,
 		intervals: 'articles'
 	},
 	{
-		code: 'w4',
+		id: 'w4',
 		talent: 'wisdom',
 		terms: 'Call someone you love and talk to them for at least',
 		count: 4,
 		intervals: 'minutes'
 	},
 	{
-		code: 'w5',
+		id: 'w5',
 		talent: 'wisdom',
 		terms: 'Google',
 		count: 5,
 		intervals: 'interests'
 	},
 	{
-		code: 'w6',
+		id: 'w6',
 		talent: 'wisdom',
 		terms: 'Engage philosophy, google someone for',
 		count: 6,
 		intervals: 'minutes'
 	},
 	{
-		code: 'w7',
+		id: 'w7',
 		talent: 'wisdom',
 		terms: 'Enjoy scenary, stare through the window for',
 		count: 7,
 		intervals: 'minutes'
 	},
 	{
-		code: 'w8',
+		id: 'w8',
 		talent: 'wisdom',
 		terms: 'self-reflect, look at a mirror for',
 		count: 8,
 		intervals: 'minutes'
 	},
 	{
-		code: 'w9',
+		id: 'w9',
 		talent: 'wisdom',
 		terms: 'find a self-care book and read ',
 		count: 9,
 		Intervals: 'Pages'
 	},
 	{
-		code: 'w10',
+		id: 'w10',
 		talent: 'wisdom',
 		terms: 'write about',
 		count: 10,
 		intervals: 'things'
 	},
 	{
-		code: 'w11',
+		id: 'w11',
 		talent: 'wisdom',
 		terms: 'write about your day has affected you',
 		count: 11,
 		intervals: 'minutes'
 	},
 	{
-		code: 'w12',
+		id: 'w12',
 		talent: 'wisdom',
 		terms: 'Write different things you love for ',
 		count: 12,
 		intervals: 'counts'
 	},
 	{
-		code: 'w13',
+		id: 'w13',
 		talent: 'wisdom',
 		terms: 'Incite wisdom to a friend, talk to them for',
 		count: 13,
 		intervals: 'meditate'
 	},
 	{
-		code: 's1',
+		id: 's1',
 		talent: 'spirit',
 		terms: 'Tell someone them how much you care about them and what they have done to improve your life for',
 		count: 1,
 		intervals: 'phonecall'
 	},
 	{
-		code: 's2',
+		id: 's2',
 		talent: 'spirit',
 		terms: 'pray',
 		count: 2,
 		intervals: 'minutes'
 	},
 	{
-		code: 's3',
+		id: 's3',
 		talent: 'spirit',
 		terms: 'Find some space and meditate for',
 		count: 3,
 		intervals: 'minutes'
 	},
 	{
-		code: 's4',
+		id: 's4',
 		talent: 'spirit',
 		terms: 'Read about a spiritual article',
 		count: 4,
 		intervals: 'minutes'
 	},
 	{
-		code: 's5',
+		id: 's5',
 		talent: 'spirit',
 		terms: 'pray for',
 		count: 5,
 		intervals: 'minutes'
 	},
 	{
-		code: 's6',
+		id: 's6',
 		talent: 'spirit',
 		terms: 'write down good things that have happened to you today',
 		count: 6,
 		intervals: 'items'
 	},
 	{
-		code: 's7',
+		id: 's7',
 		talent: 'spirit',
 		terms: 'Meditate for',
 		count: 7,
 		intervals: 'pages'
 	},
 	{
-		code: 's8',
+		id: 's8',
 		talent: 'spirit',
 		terms: 'Count your blessings and write them for',
 		count: 8,
 		intervals: 'minutes'
 	},
 	{
-		code: 's9',
+		id: 's9',
 		talent: 'spirit',
 		terms: 'Light a candle and stare at it for',
 		count: 9,
 		intervals: 'minutes'
 	},
 	{
-		code: 's10',
+		id: 's10',
 		talent: 'spirit',
 		terms: 'List what you are grateful for',
 		count: 10,
 		intervals: 'nouns'
 	},
 	{
-		code: 's11',
+		id: 's11',
 		talent: 'spirit',
 		terms: 'Write to the world for',
 		count: 11,
 		intervals: 'minutes'
 	},
 	{
-		code: 's12',
+		id: 's12',
 		talent: 'spirit',
 		terms: 'do guided meditation for',
 		count: 12,
 		intervals: 'chapters'
 	},
 	{
-		code: 's13',
+		id: 's13',
 		talent: 'spirit',
 		terms: 'do 5 downward dogs for',
 		count: 13,
 		intervals: 'seconds each'
 	},
 	{
-		code: 'v1',
+		id: 'v1',
 		talent: 'Vitality',
 		terms: 'Pace the Earth, Run for',
 		count: 1,
 		intervals: 'Miles'
 	},
 	{
-		code: 'v2',
+		id: 'v2',
 		talent: 'Vitality',
 		terms: 'Do pushups for a total of ',
 		count: 2,
 		intervals: 'minutes'
 	},
 	{
-		code: 'v3',
+		id: 'v3',
 		talent: 'Vitality',
 		terms: 'Hug the floor, do planks for',
 		count: 3,
 		intervals: 'minutes'
 	},
 	{
-		code: 'v4',
+		id: 'v4',
 		talent: 'Vitality',
 		terms: 'Do sit ups for',
 		count: 4,
 		intervals: 'Reps of 10'
 	},
 	{
-		code: 'v5',
+		id: 'v5',
 		talent: 'Vitality',
 		terms: 'Jump rope',
 		count: 5,
 		intervals: 'Reps of 20'
 	},
 	{
-		code: 'v6',
+		id: 'v6',
 		talent: 'Vitality',
 		terms: 'Do burpees',
 		count: 6,
 		intervals: 'Reps of 5'
 	},
 	{
-		code: 'v7',
+		id: 'v7',
 		talent: 'Vitality',
 		terms: 'Do cat and cow stretch',
 		count: 7,
 		intervals: 'times'
 	},
 	{
-		code: 'v8',
+		id: 'v8',
 		talent: 'Vitality',
 		terms: 'Drink glass of water',
 		count: 8,
 		intervals: 'ounces'
 	},
 	{
-		code: 'v9',
+		id: 'v9',
 		talent: 'Vitality',
 		terms: 'Do squats',
 		count: 9,
 		intervals: 'Reps of 5'
 	},
 	{
-		code: 'v10',
+		id: 'v10',
 		talent: 'Vitality',
 		terms: 'Stretch',
 		count: 10,
 		intervals: 'Minutes'
 	},
 	{
-		code: 'v11',
+		id: 'v11',
 		talent: 'Vitality',
 		terms: 'Touch your toes',
 		count: 11,
 		intervals: 'Times'
 	},
 	{
-		code: 'v12',
+		id: 'v12',
 		talent: 'Vitality',
 		terms: 'Do jump squats for',
 		count: 12,
 		intervals: 'Reps of 2'
 	},
 	{
-		code: 'v13',
+		id: 'v13',
 		talent: 'Vitality',
 		terms: 'Do slow calf raises for',
 		count: 13,
 		intervals: 'Sets of 2'
 	},
 	{
-		code: 'm1',
+		id: 'm1',
 		talent: 'Mind',
 		terms: 'Learn something new and read ',
 		count: 1,
 		intervals: 'article'
 	},
 	{
-		code: 'm2',
+		id: 'm2',
 		talent: 'Mind',
 		terms: 'Learn something new about your hobby and read',
 		count: 2,
 		intervals: 'articles'
 	},
 	{
-		code: 'm3',
+		id: 'm3',
 		talent: 'Mind',
 		terms: 'Learn something new and read ',
 		count: 3,
 		intervals: 'article'
 	},
 	{
-		code: 'm4',
+		id: 'm4',
 		talent: 'Mind',
 		terms: 'Google a riddle and try to solve it in ',
 		count: 4,
 		intervals: 'minutes'
 	},
 	{
-		code: 'm5',
+		id: 'm5',
 		talent: 'Mind',
 		terms: 'Read a non-fiction book for ',
 		count: 5,
 		intervals: 'minutes'
 	},
 	{
-		code: 'm6',
+		id: 'm6',
 		talent: 'Mind',
 		terms: 'Read a non-fiction book for',
 		count: 6,
 		intervals: 'minutes'
 	},
 	{
-		code: 'm7',
+		id: 'm7',
 		talent: 'Mind',
 		terms: 'Learn something new and read articles for ',
 		count: 7,
 		intervals: 'minutes'
 	},
 	{
-		code: 'm8',
+		id: 'm8',
 		talent: 'Mind',
 		terms: 'Teach something to someone for',
 		count: 8,
 		intervals: 'minutes'
 	},
 	{
-		code: 'm9',
+		id: 'm9',
 		talent: 'Mind',
 		terms: 'Have someone teach you something for',
 		count: 9,
 		intervals: 'minutes'
 	},
 	{
-		code: 'm10',
+		id: 'm10',
 		talent: 'Mind',
 		terms: 'Read a non-fiction book for',
 		count: 10,
 		intervals: 'minutes'
 	},
 	{
-		code: 'm11',
+		id: 'm11',
 		talent: 'Mind',
 		terms: 'Read a non-fiction book for',
 		count: 11,
 		intervals: 'minutes'
 	},
 	{
-		code: 'm12',
+		id: 'm12',
 		talent: 'Mind',
 		terms: 'Read either 1 or',
 		count: 12,
 		intervals: 'chapters'
 	},
 	{
-		code: 'm13',
+		id: 'm13',
 		talent: 'Mind',
 		terms: 'Read 3 articles or read for',
 		count: 13,
